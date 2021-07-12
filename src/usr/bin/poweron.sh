737b96cbf2b769ecb1787a730160db4cbe381b7a
Angel Ivan Castell Rovira <al004140@gmail.com>
2021-07-12 18:06:27 +0200

    tiny cosmetics

--- src/usr/bin/poweron.sh
+++ src/usr/bin/poweron.sh
@@ -4,25 +4,25 @@ source /etc/rpi-rele.conf
 
 export_gpio_dir()
 {
-	GPIO_NR=$1
-	GPIO_DIR=$2
-	if [ ! -d /sys/class/gpio/gpio${GPIO_NR} ]
-	then
-		echo $GPIO_NR > /sys/class/gpio/export
-		echo $GPIO_DIR > /sys/class/gpio/gpio${GPIO_NR}/direction
-	fi
+    GPIO_NR=$1
+    GPIO_DIR=$2
+    if [ ! -d /sys/class/gpio/gpio${GPIO_NR} ]
+    then
+        echo $GPIO_NR > /sys/class/gpio/export
+        echo $GPIO_DIR > /sys/class/gpio/gpio${GPIO_NR}/direction
+    fi
 }
 
 off_gpio()
 {
-	GPIO_NR=$1
-	echo 1 > /sys/class/gpio/gpio${GPIO_NR}/value
+    GPIO_NR=$1
+    echo 1 > /sys/class/gpio/gpio${GPIO_NR}/value
 }
 
 on_gpio()
 {
-	GPIO_NR=$1
-	echo 0 > /sys/class/gpio/gpio${GPIO_NR}/value
+    GPIO_NR=$1
+    echo 0 > /sys/class/gpio/gpio${GPIO_NR}/value
 }
 
 # Export some GPIOS and set direction
--- src/usr/bin/reset.sh
+++ src/usr/bin/reset.sh
@@ -4,19 +4,19 @@ source /etc/rpi-rele.conf
 
 export_gpio_dir()
 {
-	GPIO_NR=$1
-	GPIO_DIR=$2
-	if [ ! -d /sys/class/gpio/gpio${GPIO_NR} ]
-	then
-		echo $GPIO_NR > /sys/class/gpio/export
-		echo $GPIO_DIR > /sys/class/gpio/gpio${GPIO_NR}/direction
-	fi
+    GPIO_NR=$1
+    GPIO_DIR=$2
+    if [ ! -d /sys/class/gpio/gpio${GPIO_NR} ]
+    then
+        echo $GPIO_NR > /sys/class/gpio/export
+        echo $GPIO_DIR > /sys/class/gpio/gpio${GPIO_NR}/direction
+    fi
 }
 
 off_gpio()
 {
-	GPIO_NR=$1
-	echo 1 > /sys/class/gpio/gpio${GPIO_NR}/value
+    GPIO_NR=$1
+    echo 1 > /sys/class/gpio/gpio${GPIO_NR}/value
 }
 
 on_gpio()
@@ -35,5 +35,4 @@ off_gpio $GPIO_RST1
 on_gpio $GPIO_RST1
 sleep 1
 off_gpio $GPIO_RST1
-
 
