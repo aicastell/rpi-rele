54de97451a2808ce2ad0799cc3b366a20bec2b6b
Angel Ivan Castell Rovira <al004140@gmail.com>
2021-07-13 16:56:27 +0200

    set permissions to installed scripts

--- install.sh
+++ install.sh
@@ -2,5 +2,10 @@
 
 echo "Installing rpi-rele software"
 cp -Rfa src/* /
-echo "Done"
 
+echo "Settings permissions"
+chmod +x /usr/bin/poweron.sh
+chmod +x /usr/bin/reset.sh
+chmod +x /usr/bin/rig-monit.sh
+
+echo "Done"
