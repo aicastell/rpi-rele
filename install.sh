d21c403fbf67d02ea856ef0bb8274a1b4998f369
Angel Ivan Castell Rovira <al004140@gmail.com>
2021-07-16 16:58:04 +0200

    improve install script

--- install.sh
+++ install.sh
@@ -1,11 +1,32 @@
 #! /bin/bash
 
+APT_DEPENDS="git vim htop"
+
+check_apt_depends()
+{
+    echo "Checking package dependencies..."
+    for DEPEND in ${APT_DEPENDS}
+    do  
+        dpkg -s ${DEPEND} &> /dev/null 2>&1
+        if [ $? -eq 0 ]; then
+            echo "Package depend ${DEPEND} is installed" &> /dev/null 2>&1
+        else
+            sudo apt install ${DEPEND}
+        fi
+    done
+}
+
+##### MAIN
+
+echo "Checking apt dependencies"
+check_apt_depends
+
 echo "Installing rpi-rele software"
-cp -Rfa src/* /
+sudo cp -Rfa src/* /
 
 echo "Settings permissions"
-chmod +x /usr/bin/poweron.sh
-chmod +x /usr/bin/reset.sh
-chmod +x /usr/bin/rig-monit.sh
+sudo chmod +x /usr/bin/poweron.sh
+sudo chmod +x /usr/bin/reset.sh
+sudo chmod +x /usr/bin/rig-monit.sh
 
 echo "Done"
