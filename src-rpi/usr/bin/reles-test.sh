cd171b0958f90fd532bbfd293cf90f0108222ecc
Angel Ivan Castell Rovira <al004140@gmail.com>
2021-07-30 16:42:50 +0200

    reles-test improved

--- src-rpi/usr/bin/reles-test.sh
+++ src-rpi/usr/bin/reles-test.sh
@@ -1,35 +1,45 @@
 #! /bin/bash
 
 echo 4 > /sys/class/gpio/export
+sleep 1
 echo 22 > /sys/class/gpio/export
+sleep 1
 echo 23 > /sys/class/gpio/export
+sleep 1
 echo 27 > /sys/class/gpio/export
+sleep 1
 
 echo out > /sys/class/gpio/gpio4/direction
+sleep 1
 echo out > /sys/class/gpio/gpio22/direction
+sleep 1
 echo out > /sys/class/gpio/gpio23/direction
+sleep 1
 echo out > /sys/class/gpio/gpio27/direction
+sleep 1
 
 
 while [ 1 ]
 do
+    # Apagados
+    echo 1 > /sys/class/gpio/gpio4/value
+    sleep 1
+    echo 1 > /sys/class/gpio/gpio22/value
+    sleep 1
+    echo 1 > /sys/class/gpio/gpio23/value
+    sleep 1
+    echo 1 > /sys/class/gpio/gpio27/value
+    sleep 1
 
-# Apagados
-echo 1 > /sys/class/gpio/gpio4/value
-echo 1 > /sys/class/gpio/gpio22/value
-echo 1 > /sys/class/gpio/gpio23/value
-echo 1 > /sys/class/gpio/gpio27/value
-
-sleep 2
-
-
-# Encendidos
-
-echo 0 > /sys/class/gpio/gpio4/value
-echo 0 > /sys/class/gpio/gpio22/value
-echo 0 > /sys/class/gpio/gpio23/value
-echo 0 > /sys/class/gpio/gpio27/value
-
+    # Encendidos
+    echo 0 > /sys/class/gpio/gpio4/value
+    sleep 1
+    echo 0 > /sys/class/gpio/gpio22/value
+    sleep 1
+    echo 0 > /sys/class/gpio/gpio23/value
+    sleep 1
+    echo 0 > /sys/class/gpio/gpio27/value
+    sleep 1
 done
 
 
